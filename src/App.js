7090a197ac111d8b072580bd0ff7fda96be7d9d0
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-05-12 02:45:41 +0500

    api added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Cards from "./components/Cards/Cards";
 import AppFooter from "./components/AppFooter/AppFooter";
 import logo from "./logo.png";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="Logo" style={{ height: "100px" }} />
-      </header>
-      <Cards />
-      <br />
-      <h1 style={{ color: "white" }}>More functionalities will added soon.</h1>
-      <footer className="App-footer">
-        <AppFooter />
-      </footer>
-    </div>
-  );
+import { fetchData } from "./api";
+
+class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  render() {
+    // const { data } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="Logo" style={{ height: "100px" }} />
+        </header>
+        {console.log("this is app.js ", this.state.data)}
+        <Cards />
+        <br />
+        <h1 style={{ color: "white" }}>
+          More functionalities will added soon.
+        </h1>
+        <footer className="App-footer">
+          <AppFooter />
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "./Cards.css";
 
-const Cards = () => {
+const Cards = (data) => {
   return (
     <div className="cards">
       <div className="card cases hoverable">
         <b>Total Cases</b>
-        <h1>00000</h1>
+        <h1>Real Data</h1>
         <b>Wed Apr 01, 2020</b>
         <p>Number of active cases of COVID-19.</p>
       </div>
       <div className="card recovers hoverable">
         <b>Recoverds</b>
-        <h1>000000000000</h1>
+        <h1>Real Data</h1>
         <b>Wed Apr 01, 2020</b>
         <p>Number of recoveries from COVID-19.</p>
       </div>
       <div className="card deaths hoverable">
         <b>Deaths</b>
-        <h1>0000</h1>
+        <h1>Real Data</h1>
         <b>Wed Apr 01, 2020</b>
         <p>Number of deaths caused by COVID-19.</p>
       </div>
