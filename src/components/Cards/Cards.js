0fe6bca808d7ffbad5647c7c42afc1538c3a6565
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-05-12 03:45:09 +0500

    CountUp added and lastupdate added

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,26 +4,46 @@ import CountUp from "react-countup";
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   if (!confirmed) {
-    return "Loading";
+    return (
+      <div className="cards">
+        <h1 style={{ color: "white", fontSize: "32px" }}>Loading....</h1>
+      </div>
+    );
   }
   return (
     <div className="cards">
       <div className="card cases hoverable">
         <b>Total Cases</b>
-        <h1>{confirmed.value}</h1>
-        <b>Wed Apr 01, 2020</b>
+        <h1>
+          <CountUp
+            start={0}
+            end={confirmed.value}
+            duration={2.5}
+            separator=","
+          />
+        </h1>
+        <b>{new Date(lastUpdate).toDateString()}</b>
         <p>Number of confirmed cases of COVID-19.</p>
       </div>
       <div className="card recovers hoverable">
         <b>Recoverds</b>
-        <h1>{recovered.value}</h1>
-        <b>Wed Apr 01, 2020</b>
+        <h1>
+          <CountUp
+            start={0}
+            end={recovered.value}
+            duration={2.5}
+            separator=","
+          />
+        </h1>
+        <b>{new Date(lastUpdate).toDateString()}</b>
         <p>Number of recoveries from COVID-19.</p>
       </div>
       <div className="card deaths hoverable">
         <b>Deaths</b>
-        <h1>{deaths.value}</h1>
-        <b>Wed Apr 01, 2020</b>
+        <h1>
+          <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+        </h1>
+        <b>{new Date(lastUpdate).toDateString()}</b>
         <p>Number of deaths caused by COVID-19.</p>
       </div>
     </div>
